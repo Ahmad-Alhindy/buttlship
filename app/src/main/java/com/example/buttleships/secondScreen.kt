7a859d21328ec69f7feadf717437607301e9bff0
Ahmad Alhindy <alah23xc@student.ju.se>
2024-11-14 08:21:20 +0100

    deleting the unnecessary import

--- app/src/main/java/com/example/buttleships/secondScreen.kt
+++ app/src/main/java/com/example/buttleships/secondScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
